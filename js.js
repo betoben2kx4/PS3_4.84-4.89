a9958fd3925e126477434a65a663a99e2d38ddb5
Bishal Hasan Bhuiyan <94838617+bishalqx980@users.noreply.github.com>
2022-09-26 21:42:33 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,30 @@
+function showbtn() {
+	var show = document.getElementById('hiddenbtn');
+	if (show.style.display == "") {
+		show.style.display = "none"
+	}
+	else {
+		show.style.display = ""
+    }	
+	var show = document.getElementById('hiddenbtn1');
+	if (show.style.display == "") {
+		show.style.display = "none"
+	}
+	else {
+		show.style.display = ""
+    }
+	var show = document.getElementById('hiddenbtn2');
+	if (show.style.display == "") {
+		show.style.display = "none"
+	}
+	else {
+		show.style.display = ""
+    }		
+	var show = document.getElementById('hiddenbtn3');
+	if (show.style.display == "") {
+		show.style.display = "none"
+	}
+	else {
+		show.style.display = ""
+	}
+}
